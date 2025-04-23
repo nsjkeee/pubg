f13d7e5da8ea0928037097df05a23baa6b92b326
nsjkeee <nsjkeee@gmail.com>
2025-04-23 15:20:35 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// src/App.tsx
+import React from "react";
+import HamsterBattle from "./HamsterBattle";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 p-4 text-white">
+      <HamsterBattle />
+    </div>
+  );
+}
